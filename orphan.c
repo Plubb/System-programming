1943c146851702dd306f800442c0742915cb6642
Plubb <107609140+Plubb@users.noreply.github.com>
2024-11-01 23:32:10 +0700

    Update orphan.c

--- orphan.c
+++ orphan.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <pthread.h>
-#include <signal.h>
 
-int main(){
-  int pid = fork();
+int main() {
+    int pid = fork();
 
-  if(pid){
-    sleep(3);
-    printf("This is Parent\n");
-  } else{
-    sleep(6);
-    while(1){
-      printf("This is Child\n");
-      sleep(1);
+    if (pid > 0) { // Parent process
+        sleep(3); // Parent sleeps for 3 seconds
+        printf("Parent process exiting...\n");
+        exit(0); // Parent exits, making the child an orphan
+    } else if (pid == 0) { // Child process
+        sleep(6); // Child sleeps for 6 seconds before printing
+        while (1) {
+            printf("This is Child\n");
+            sleep(1); // Child continues to run indefinitely
+        }
+    } else {
+        fprintf(stderr, "Fork failed!\n");
+        return 1;
     }
-  }
-  return 0;
+
+    return 0;
 }
